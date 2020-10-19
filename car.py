109a7c51f0af1b1f7fb12451a5d0b0b977cfa8fc
Adrian Macneil <adrian@adrianmacneil.com>
2020-10-18 21:38:19 -0700

    Add speed limit

--- car.py
+++ car.py
@@ -4,21 +4,34 @@ from adafruit_motorkit import MotorKit
 ACCELERATION = 0.5
 MOVE_DURATION = 0.1
 
+# MAX_SPEED prevents the motors from killing raspberry pi voltage
+MAX_SPEED = 0.8
+
+
+def limit_speed(speed):
+    if speed > MAX_SPEED:
+        speed = MAX_SPEED
+    elif speed < -MAX_SPEED:
+        speed = -MAX_SPEED
+
+    return speed
+
 
 class Car:
     def __init__(self, logger):
         self.logger = logger
         self.kit = MotorKit()
+        self.motors = {
+            "lf": self.kit.motor3,
+            "rf": self.kit.motor4,
+            "lr": self.kit.motor1,
+            "rr": self.kit.motor2,
+        }
+
         self.stop()
 
     def go(self, speed):
-        # sensible speed limit
-        if speed > 1:
-            speed = 1
-        elif speed < -1:
-            speed = -1
-
-        self.speed = float(speed)
+        self.speed = limit_speed(float(speed))
         self.set_throttle(self.speed, self.speed, self.speed, self.speed)
 
     def stop(self):
@@ -41,29 +54,31 @@ class Car:
             self.go(self.speed - ACCELERATION)
 
     def forward(self):
-        self.move(1, 1, 1, 1, MOVE_DURATION)
+        self.move(MAX_SPEED, MAX_SPEED, MAX_SPEED, MAX_SPEED, MOVE_DURATION)
 
     def back(self):
-        self.move(-1, -1, -1, -1, MOVE_DURATION)
+        self.move(-MAX_SPEED, -MAX_SPEED, -MAX_SPEED, -MAX_SPEED, MOVE_DURATION)
 
     def left(self):
-        self.move(-1, 1, -1, 1, MOVE_DURATION)
+        self.move(-MAX_SPEED, MAX_SPEED, -MAX_SPEED, MAX_SPEED, MOVE_DURATION)
 
     def right(self):
-        self.move(1, -1, 1, -1, MOVE_DURATION)
+        self.move(MAX_SPEED, -MAX_SPEED, MAX_SPEED, -MAX_SPEED, MOVE_DURATION)
 
     def move(self, lf, rf, lr, rr, duration):
-        orig_lf = self.kit.motor3.throttle
-        orig_rf = self.kit.motor4.throttle
-        orig_lr = self.kit.motor1.throttle
-        orig_rr = self.kit.motor2.throttle
-
+        prev = self.get_throttle()
         self.set_throttle(lf, rf, lr, rr)
         time.sleep(duration)
-        self.set_throttle(orig_lf, orig_rf, orig_lr, orig_rr)
+        self.set_throttle(*prev)
+
+    def get_throttle(self):
+        return (
+            self.motors["lf"].throttle,
+            self.motors["rf"].throttle,
+            self.motors["lr"].throttle,
+            self.motors["rr"].throttle,
+        )
 
     def set_throttle(self, lf, rf, lr, rr):
-        self.kit.motor3.throttle = lf
-        self.kit.motor4.throttle = rf
-        self.kit.motor1.throttle = lr
-        self.kit.motor2.throttle = rr
+        for name, speed in [("lf", lf), ("rf", rf), ("lr", lr), ("rr", rr)]:
+            self.motors[name].throttle = limit_speed(speed)
